bc47724954a78cac49815cb1f0893604d670098f
Ratan <ratan@Ratans-MacBook-Pro.local>
2016-10-26 19:49:35 +0530

    Fixed server host and port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const app = require('./app');
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3022);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 const port = app.get('port');
-const server = app.listen(port);
+const ip = app.get('ip');
+
+const server = app.listen(port, ip);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Feathers application started on ${ip}:${port}`)
 );
